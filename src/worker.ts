1d21871a4522ae7a5d98ab4e82df48303f1c08ee
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2021-10-12 17:26:43 -0400

    adds localstorage to msw

--- src/worker.ts
+++ src/worker.ts
@@ -1,20 +1,42 @@
 import { rest, setupWorker } from "msw";
 import { Friend } from "./friend/friend.interface";
 
-let friends: Friend[] = [
-  {
-    id: "1",
-    name: "Bob",
-    age: 43,
-  },
-  {
-    id: "2",
-    name: "Suzy",
-    age: 53,
-  },
-];
+function incrementCurrentIndex(): void {
+  let currentIndex = getCurrentIndex();
+  currentIndex++;
+  window.localStorage.setItem("currentIndex", JSON.stringify(currentIndex));
+}
 
-let currentId = 3;
+function getCurrentIndex(): number {
+  let value = window.localStorage.getItem("currentIndex");
+
+  if (value === undefined || value === null) {
+    value = "1";
+    window.localStorage.setItem("currentIndex", value);
+  }
+
+  return +value;
+}
+
+function setFriends(friends: Friend[]): void {
+  window.localStorage.setItem("friends", JSON.stringify(friends));
+}
+
+function getFriends(): Friend[] {
+  let value: any = window.localStorage.getItem("friends");
+  let friends: Friend[];
+
+  try {
+    friends = JSON.parse(value) ?? [];
+  } catch (e) {
+    window.localStorage.setItem("friends", JSON.stringify([]));
+    friends = [];
+  }
+
+  return friends;
+}
+
+let friends = getFriends();
 const delayLength = 0;
 
 export const worker = setupWorker(
@@ -23,9 +45,10 @@ export const worker = setupWorker(
   }),
   rest.post("/api/friends", (req, res, ctx) => {
     const { name, age } = req.body as Friend;
-    const friend: Friend = { id: `${currentId}`, name, age };
+    const friend: Friend = { id: `${getCurrentIndex()}`, name, age };
     friends.push(friend);
-    currentId++;
+    setFriends(friends);
+    incrementCurrentIndex();
     return res(ctx.delay(delayLength), ctx.json({ friend }));
   }),
   rest.patch("/api/friends/:id", (req, res, ctx) => {
@@ -34,11 +57,13 @@ export const worker = setupWorker(
     const friend: Friend = { id, name, age };
     let index = friends.findIndex((friend) => friend.id === id);
     friends[index] = friend;
+    setFriends(friends);
     return res(ctx.delay(delayLength), ctx.json({ friend }));
   }),
   rest.delete("/api/friends/:id", (req, res, ctx) => {
     const id = req.params.id;
     friends = friends.filter((friend) => friend.id !== id);
+    setFriends(friends);
     return res(ctx.delay(delayLength), ctx.status(204));
   })
 );
